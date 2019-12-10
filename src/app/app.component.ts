015a30999687b5f9d4d85a551c6105c6ab959ec2
Gulshan Kumar <glsunny.kumar@gmail.com>
2019-12-10 10:00:10 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  teams$ :Observable<any> = this.http.get('/api/teams');
+
+  constructor(private http: HttpClient){}
 }

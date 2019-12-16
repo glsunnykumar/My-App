9aeb331154e333aef8bf9a91a2a0f8aec0af8d9a
Gulshan Kumar <glsunny.kumar@gmail.com>
2019-12-16 14:47:30 +0530

    Header File

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import  {HeaderComponent} from './Header/header.component';
+import { ReceipesComponent } from './receipes/receipes.component';
+import { ReceipeListComponent } from './Receipes/receipe-list/receipe-list.component';
+import { ReceipeDetailComponent } from './Receipes/receipe-detail/receipe-detail.component';
+import { ReceipeItemComponent } from './Receipes/receipe-list/receipe-item/receipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingeditComponent } from './shopping-list/shoppingedit/shoppingedit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ReceipesComponent,
+    ReceipeListComponent,
+    ReceipeDetailComponent,
+    ReceipeItemComponent,
+    ShoppingListComponent,
+    ShoppingeditComponent
+
   ],
   imports: [
     BrowserModule,
